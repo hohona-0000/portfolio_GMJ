dcd84c2dcb32010f78857ea7d7d4086ce278a330
ho_GMJ <146080852+hohona-0000@users.noreply.github.com>
2024-05-02 13:55:07 +0900

    Add files via upload

---
+++ script/na.js
@@ -0,0 +1,30 @@
+//submenu
+$(".coco").mouseover(function(){$(".submenu").stop().slideDown();})
+.mouseout(function(){$(this).find(".submenu").stop().slideUp();});
+
+
+//modal
+
+$(".il1").click(function(){$(".modal1").show()});
+$(".button button").click(function(){$(".modal1").hide()});
+
+$(".il2").click(function(){$(".modal2").show()});
+$(".button button").click(function(){$(".modal2").hide()});
+
+$(".il4").click(function(){$(".modal4").show()});
+$(".button button").click(function(){$(".modal4").hide()});
+
+$(".il3").click(function(){$(".modal3").show()});
+$(".button button").click(function(){$(".modal3").hide()});
+
+$(".il5").click(function(){$(".modal5").show()});
+$(".button button").click(function(){$(".modal5").hide()});
+
+$(".il6").click(function(){$(".modal6").show()});
+$(".button button").click(function(){$(".modal6").hide()});
+
+$(".il7").click(function(){$(".modal7").show()});
+$(".button button").click(function(){$(".modal7").hide()});
+
+$(".po").click(function(){$(".modal8").show()});
+$(".button button").click(function(){$(".modal8").hide()});
